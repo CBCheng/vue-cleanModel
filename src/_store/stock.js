1e334b3a36b15b1f0fb774e2528e9d7892490027
Cheng <cye1326@gmail.com>
2020-03-16 17:36:11 +0800

    API處理 暫時 1.0.2

--- src/_store/stock.js
+++ src/_store/stock.js
@@ -1,31 +1,39 @@
-import { createTypes, createGetters, createMutations } from './_vuexTool';
-import requestSync from './_vuexApi'
-
+// This is a template of vuex module
+import { createTypes, createGetters, createMutations } from '@/_store/_vuexTool.js';
+import adminRequest from '@/_store/_adminApi.js'
 
 const types = createTypes([
-    "getActiveClientList",
+    "getOptions",
+    "getOptionById",
 ])
 
 const state = {
-    // runLoading:false
+
 }
 
 const getters = {
-    ...createGetters(types),
+    ...createGetters(types)
 }
 
 const actions = {
-    getActiveClientList: async (store, datas) => {
-        const { data, id } = datas;
-        store.commit("setLoading", true);
-        const response = await requestSync(store, types.getActiveClientList, {
-            url: `/Customer/ActiveClientAccountList/${id}`,
-            data,
+    getOptions: async (store) => {
+        store.commit('setLoading', true);
+        const reponse = await adminRequest(types.getOptions, {
+            url: '/v1/Options',
         })
-        store.commit("setLoading", false);
-        return response
+        store.commit('setLoading', false);
+        return reponse
+    },
+    getOptionById: async (store, OptID) => {
+        store.commit('setLoading', true);
+        const reponse = await adminRequest(types.getOptionById, {
+            url: `/v1/Options/${OptID}`,
+        })
+        store.commit('setLoading', false);
+        return reponse
     },
 }
+
 const mutations = {
     ...createMutations(types)
 }
